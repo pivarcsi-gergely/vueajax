7d7949c18e3bd7460cc4333395d69fbd5576a8ff
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2022-01-25 15:49:12 +0100

    fixed component implementation + vue router

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,16 @@
 <template>
   <div id="app">
-
     <table>
-
       <thead>
         <tr>
-          <th>Azonsító</th>
-          <th>Személy</th>
-          <th>Magasság</th>
-          <th>Érték</th>
-          <th>Műveletek</th>
+          <th>ID</th>
+          <th>Person</th>
+          <th>Height</th>
+          <th>Price</th>
         </tr>
       </thead>
-
-      <tbody>
-        
-        <tr v-for="statue in statues" v-bind:key="statue.id">
-          <td>{{ statue.id }}</td>
-          <td>{{ statue.person }}</td>
-          <td>{{ statue.height }}</td>
-          <td>{{ statue.price }}</td>
-          <td>
-            <button @click="deleteStatue(statue.id)">Törlés</button>
-            <button @click="editStatue(statue.id)">Szerkesztés</button>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <input type="hidden" v-model="statue.id">
-          </td>
-          <td>
-            <input type="text" v-model="statue.person">
-          </td>
-          <td>
-            <input type="number" v-model="statue.height">
-          </td>
-          <td>
-            <input type="number" v-model="statue.price">
-          </td>
-          <td>
-            <button v-if="mod_new" @click="newStatue" :disabled="saving">Létrehoz</button>
-            <button v-if="!mod_new" @click="saveStatue" :disabled="saving">Mentés</button>
-            <button v-if="!mod_new" @click="cancelEdit" :disabled="saving">Mégse</button>
-          </td>
-        </tr>
-        
-      </tbody>
-
+      <router-view></router-view>
     </table>
-
-    <button @click="loadData">Adatok betöltése</button>
   </div>
 </template>
 
@@ -59,90 +19,13 @@
 export default {
   name: 'App',
   components: {
-  },
+},
   data() {
     return {
-      mod_new: true, 
-      saving: false,
-      statue: {
-        id: null,
-        person: '',
-        height: null,
-        price: null
-      },
-      statues: []
     }
   },
   methods: {
-    async loadData () {
-     let Response = await fetch('http://127.0.0.1:8000/api/statues')
-     let data = await Response.json()
-     this.statues = data
-    },
-    
-    async deleteStatue(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`, {
-        method: 'DELETE'
-      })
-      console.log(Response)
-      await this.loadData()
-    },
-
-    async newStatue() {
-      this.saving='disabled'
-     await fetch('http://127.0.0.1:8000/api/statues', {
-       method: 'POST',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.statue) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-
-    async saveStatue() {
-      this.saving='disabled'
-     await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
-       method: 'PATCH',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.statue) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-
-    async editStatue(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
-      let data = await Response.json()
-      this.statue = {...data};
-      this.mod_new = false
-    },
-
-    cancelEdit () {
-      this.resetForm()
-    },
-
-    resetForm() {
-      this.statue = {
-        id: null,
-        person: '',
-        height: null,
-        price: null
-      }
-      this.mod_new = true
-    }
-  },
-
-  mounted() {
-    this.loadData()
-  }
+}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import StatueItem from './components/StatueItem.vue';
+import Info from './components/Info.vue';
+
+Vue.use(VueRouter)
+
 
 Vue.config.productionTip = false
 
+const routes = [
+  {
+  path: '/',
+  component: StatueItem
+  },
+  {
+    path: '/info',
+    component: Info
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
